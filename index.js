6a6eabc99382f5d7eb65dc854fd78f6526d4f817
Alessandro Amarri <amarri.alessandro@gmail.com>
2020-07-23 18:21:42 +0100

    Iteration 6 done

--- index.js
+++ index.js
@@ -12,15 +12,56 @@ mongoose
   .connect(MONGODB_URI, {
     useCreateIndex: true,
     useNewUrlParser: true,
-    useUnifiedTopology: true
+    useUnifiedTopology: true,
+    useFindAndModify: false
   })
   .then(self => {
     console.log(`Connected to the database: "${self.connection.name}"`);
     // Before adding any documents to the database, let's delete all previous entries
     return self.connection.dropDatabase();
   })
+  .then(recipe => {
+    return Recipe.create({
+      title: 'Lasagne',
+      level: 'Amateur Chef',
+      ingredients: [
+        'Passata',
+        'Parmigiano',
+        'Ragu',
+        'Besciamella',
+        'Pasta fresca'
+      ],
+      cuisine: 'Italian',
+      dishType: 'main_course',
+      duration: 150,
+      creator: 'Jamies',
+      created: Date.now()
+    });
+  })
+  .then(recipe => {
+    console.log(recipe.title);
+    return Recipe.insertMany(data);
+  })
+  .then(data => {
+    for (let i = 0; i < data.length; i++) {
+      console.log(data[i].title);
+    }
+    return Recipe.findOneAndUpdate(
+      { title: 'Rigatoni alla Genovese' },
+      { duration: 100 },
+      { new: true }
+    );
+  })
+  .then(data => {
+    console.log('Duration was updated with success: ', data.duration);
+    Recipe.deleteOne({ title: 'Carrot Cake' });
+  })
+  .then(data => {
+    console.log('Carrot Cake was deleted');
+    return mongoose.disconnect();
+  })
   .then(() => {
-    // Run your code here, after you have insured that the connection was made
+    console.log('Disconnected from Mongo DB!');
   })
   .catch(error => {
     console.error('Error connecting to the database', error);
